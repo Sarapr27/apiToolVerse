917b3380e9d06ec51e20adca6323d48bb6fb3d63
diego campos <diegocrp123@gmail.com>
2023-07-11 13:56:45 -0500

    Router_controller
    Router controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const routerProduct=require('./routerProduct')
 const routerUser=require('./routerUser')
+const routerShippingAddress=require('./routerShippingAddress')
 
 const router = Router();
 
 router.use(routerProduct)
 router.use(routerUser)
+router.use(routerShippingAddress)
 
 module.exports = router;
