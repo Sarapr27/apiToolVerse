ffdda22d02fa721f3930b3dbc49536c68cd2941b
diego campos <diegocrp123@gmail.com>
2023-07-11 08:55:49 -0500

    Routes
    Se modifico las rutas y el controlador

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,8 @@
 const { Router } = require('express');
-const {getAllProducts, getProductById, getProductByName, createProducts} =require("../controllers/Product.controller");
-
+const routerProduct=require('./routerProduct')
 
 const router = Router();
 
-/* ----------------------------------- */
-/* Products                         */
-/* ----------------------------------- */
-
-router.get("/products", (req, res) => {
-    if(req.query.name) getProductByName(req, res)
-    else getAllProducts(req, res)
-});
-
-router.get("/products/:id", getProductById);
-
-router.post("/products", createProducts);
-
+router.use(routerProduct)
 
 module.exports = router;
