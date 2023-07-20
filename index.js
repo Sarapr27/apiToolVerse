109c7529e98757ce8474e1ce0b6430fac0c52400
ngpalma <nicolasgerardopalma@gmail.com>
2023-07-20 19:10:20 -0300

    deployback

--- index.js
+++ index.js
@@ -20,12 +20,13 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const {createDbTools, createDbCategories} = require("./src/Loading/loading.js")
+const {PORT} = process.env
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(async() => {
   await createDbTools();
   await createDbCategories();
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
