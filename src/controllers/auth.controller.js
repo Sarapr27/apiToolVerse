a51ae0b4b53972527408555b95b8bed9f5e59974
ChechuM <ceciliammoroni@gmail.com>
2023-07-27 14:57:05 +1000

    Update auth.controller.js
    Se amplía la info que se envía al front en la fción login -> para poder utilizar todos los datos provistos en el registro

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,22 +1,22 @@
 const { User } = require("../db");
 const bcrypt = require("bcryptjs");
 const { createAccessToken } = require("../libs/jwt");
-const jwt=require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const { TOKEN_SECRET } = require("../config");
-const nodemailer=require("nodemailer")
-const {google}=require("googleapis")
+const nodemailer = require("nodemailer")
+const { google } = require("googleapis")
 
-const CLIENT_ID="863750473318-kvh4uiflolu31cfo3eroaa8lskepul08.apps.googleusercontent.com"
-const CLIENT_SECRET="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"
-const REDIRECT_URI="https://developers.google.com/oauthplayground"
-const REFRESH_TOKEN="1//04yFomw_XoiMaCgYIARAAGAQSNwF-L9IrDo1v4oHXy4Wer4t2s7x3vDv5noC2ebO8SnNerpmHpU7NfCt8N8HNtDJDvP9neCfr7ZI"
+const CLIENT_ID = "863750473318-kvh4uiflolu31cfo3eroaa8lskepul08.apps.googleusercontent.com"
+const CLIENT_SECRET = "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"
+const REDIRECT_URI = "https://developers.google.com/oauthplayground"
+const REFRESH_TOKEN = "1//04yFomw_XoiMaCgYIARAAGAQSNwF-L9IrDo1v4oHXy4Wer4t2s7x3vDv5noC2ebO8SnNerpmHpU7NfCt8N8HNtDJDvP9neCfr7ZI"
 
 const register = async (req, res) => {
   const { email, password, firstName, lastName, role, phone } = req.body;
 
   try {
-    const userFound=await User.findOne({where:{email}})
-    if(userFound) return res.status(400).json({message:"the email already exists"})
+    const userFound = await User.findOne({ where: { email } })
+    if (userFound) return res.status(400).json({ message: "the email already exists" })
     const password_hash = await bcrypt.hash(password, 10);
 
     const user = await User.create({
@@ -30,33 +30,33 @@ const register = async (req, res) => {
     const userSaved = await user.save();
     const token = await createAccessToken({ id: userSaved.id });
     res.cookie("token", token);
-    
-    const oAuth2Client= new google.auth.OAuth2(CLIENT_ID,CLIENT_SECRET,REDIRECT_URI)
-    oAuth2Client.setCredentials({refresh_token:REFRESH_TOKEN})
 
-    const contentHtml=`
+    const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
+    oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN })
+
+    const contentHtml = `
     <h1>Bienvenido a Toolverse</h1>
     <p>Tu correo registrado es ${email}</p>
     `
-    const accesesToken=await oAuth2Client.getAccessToken()
-    const transport=nodemailer.createTransport({
-      service:"gmail",
-      auth:{
-        type:"OAuth2",
-        user:"rojas650634@gmail.com",
-        clientId:CLIENT_ID,
-        clientSecret:CLIENT_SECRET,
-        refreshToken:REFRESH_TOKEN,
-        accessToken:accesesToken
+    const accesesToken = await oAuth2Client.getAccessToken()
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: "rojas650634@gmail.com",
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        refreshToken: REFRESH_TOKEN,
+        accessToken: accesesToken
       }
     })
-    const mailOptions={
-      from:"Pagina de prueba",
-      to:email,
-      subject:"Nodemailer Prueba",
-      html:contentHtml
+    const mailOptions = {
+      from: "Pagina de prueba",
+      to: email,
+      subject: "Nodemailer Prueba",
+      html: contentHtml
     }
-    const result=await transport.sendMail(mailOptions)
+    const result = await transport.sendMail(mailOptions)
     res.json({
       id: userSaved.id,
       email: userSaved.email,
@@ -72,7 +72,7 @@ const login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const userFound = await User.findOne({ where:{email} });
+    const userFound = await User.findOne({ where: { email } });
 
     if (!userFound) return res.status(400).json({ message: "User not found" });
 
@@ -86,39 +86,42 @@ const login = async (req, res) => {
       id: userFound.id,
       email: userFound.email,
       password: userFound.password,
+      firstName: userFound.firstName,
+      lastName: userFound.lastName,
+      phone: userFound.phone
     });
   } catch (error) {
     res.status(500).json(error.message);
   }
 };
 
-const logout=(req,res)=>{
-  res.cookie('token',"",{
+const logout = (req, res) => {
+  res.cookie('token', "", {
     expires: new Date(0)
   })
   return res.sendStatus(200)
 }
-const profile=async(req,res)=>{
-  const userFound=await User.findByPk(req.user.id)
-  if(!userFound) return res.status(400).json({message:"User not found"})
+const profile = async (req, res) => {
+  const userFound = await User.findByPk(req.user.id)
+  if (!userFound) return res.status(400).json({ message: "User not found" })
   return res.json({
-    id:userFound.id,
-    email:userFound.email,
+    id: userFound.id,
+    email: userFound.email,
   })
 }
 
-const verifyToken=async(req, res) => {
-  const {token}=req.cookies
+const verifyToken = async (req, res) => {
+  const { token } = req.cookies
 
-  if(!token) return res.status(401).json({message:"Unauthorized"})
-  
-  jwt.verify(token,TOKEN_SECRET,async (err,user)=>{
-    if(err) return res.status(401).json({message:"Unauthorized"})
-    const userFound=await User.findByPk(user.id)
-    if(!userFound) return res.status(401).json({message:"Unauthorized"})
+  if (!token) return res.status(401).json({ message: "Unauthorized" })
+
+  jwt.verify(token, TOKEN_SECRET, async (err, user) => {
+    if (err) return res.status(401).json({ message: "Unauthorized" })
+    const userFound = await User.findByPk(user.id)
+    if (!userFound) return res.status(401).json({ message: "Unauthorized" })
     return res.json({
-      id:userFound.id,
-      email:userFound.email
+      id: userFound.id,
+      email: userFound.email
     })
   })
 }
