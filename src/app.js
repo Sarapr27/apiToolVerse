c52bc8577e1072f9febb928e310e45debd2be86c
JuanZu77 <juanvzubiri77@gmail.com>
2023-07-26 22:49:07 -0300

    add cookieParser()

--- src/app.js
+++ src/app.js
@@ -1,26 +1,26 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
 const routes = require('./routes/index.js');
 require('./db.js');
-const cookieParser =require('cookie-parser');
+const cors = require("cors");
+const cookieParser = require('cookie-parser');
 const server = express();
-const mercadopago = require("mercadopago")
 server.name = 'API';
 
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
-server.use(morgan('dev'));
-server.use(cookieParser())
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Reemplaza con el dominio correcto del front-end
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
 
+const corsOptions = {
+  origin: process.env.NODE_ENV === 'production' ? 'https://clienttoolverse-production.up.railway.app' : 'http://localhost:3000',
+  credentials: true,
+  methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+};
+
+server.use(cookieParser());
+
+server.use(cors(corsOptions));
+server.use(express.json());
 server.use(routes);
+
+
 
 // Error catching endware.
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
